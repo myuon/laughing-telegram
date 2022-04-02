3fc9ab1bcf8f86d7639e2ee913a35d120587ae54
myuon <ioi.joi.koi.loi@gmail.com>
2022-04-03 01:02:21 +0900

    feat: upload cover

--- src/api/storage.ts
+++ src/api/storage.ts
@@ -9,7 +9,17 @@ import {
 import useSWR from "swr";
 import { storage } from "./firebase";
 import * as id3 from "id3js";
-import { ID3Tag } from "id3js/lib/id3Tag";
+import { ID3Tag, ID3TagV2 } from "id3js/lib/id3Tag";
+
+type Metadata = Partial<ID3Tag> | null;
+
+const castMetadataAsID3TagV2 = (metadata: Metadata): ID3TagV2 | undefined => {
+  if (metadata?.["kind"] === "v2") {
+    return metadata as ID3TagV2;
+  }
+
+  return undefined;
+};
 
 export const useDownloadUrl = (key: string | undefined) => {
   return useSWR(key ? [key] : null, async () => {
@@ -27,6 +37,14 @@ export const useListAll = (key: string) => {
   });
 };
 
+export const useFetch = (key: string) => {
+  return useSWR([key], async () => {
+    const storageKey = ref(storage, `${key}.metadata`);
+    const blob = await getBlob(storageKey);
+    return JSON.parse(await blob.text()) as Partial<ID3Tag>;
+  });
+};
+
 export const useFetchMetadata = (key: string) => {
   return useSWR([key], async () => {
     const storageKey = ref(storage, `${key}.metadata`);
@@ -39,17 +57,30 @@ export const uploadMusicFileAndMetadata = async (
   userId: string,
   file: File
 ) => {
-  const metadata: object = (await id3.fromFile(file)) ?? {};
-
   const snapshot = await uploadBytes(
     ref(storage, `/user/${userId}/${file.name}`),
     file
   );
   console.log(`File uploaded`, snapshot);
 
+  const metadata = await id3.fromFile(file);
+
   const snapshotMetadata = await uploadString(
     ref(storage, `/user/${userId}/${file.name}.metadata`),
     JSON.stringify(metadata)
   );
   console.log(`Metadata uploaded`, snapshotMetadata);
+
+  const tag = castMetadataAsID3TagV2(metadata);
+  if (tag) {
+    if (tag.images.length > 0 && tag.images[0].data) {
+      await uploadBytes(
+        ref(storage, `/user/${userId}/${file.name}.cover`),
+        tag.images[0].data,
+        {
+          contentType: tag.images[0].mime ?? undefined,
+        }
+      );
+    }
+  }
 };
