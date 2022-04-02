c59cf32e9adb4b5859d9cfc07b974f7bc1391c24
myuon <ioi.joi.koi.loi@gmail.com>
2022-04-02 20:12:56 +0900

    chore: setup emotion

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from "../firebase";
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../helpers/useAuth";
+import { css } from "@emotion/react";
 
 const getRef = (userId: string) => {
   return ref(storage, `/user/${userId}/file.mp3`);
@@ -11,16 +12,19 @@ export const IndexPage = () => {
   const { userId } = useAuth();
 
   const [url, setUrl] = useState<string>();
-
   useEffect(() => {
     if (userId) {
       getDownloadURL(getRef(userId)).then(setUrl);
     }
   }, [userId]);
-  console.log(url);
 
   return userId ? (
-    <div>
+    <div
+      css={css`
+        display: grid;
+        gap: 16px;
+      `}
+    >
       <input
         type="file"
         onChange={async (event) => {
