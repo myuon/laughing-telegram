67d13e7c42500d8f810ba7591c372cb956ea8728
myuon <ioi.joi.koi.loi@gmail.com>
2022-04-06 23:33:13 +0900

    chore: setup for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
+    }),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
     }),
   ],
 });
