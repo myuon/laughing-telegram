fced78313ab72e0f7642afcdeb188dbbe5d06dd0
myuon <ioi.joi.koi.loi@gmail.com>
2022-04-07 03:21:58 +0900

    feat: force login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../api/firebase";
 import { useAuth } from "../helpers/useAuth";
@@ -8,6 +8,14 @@ const provider = new GoogleAuthProvider();
 
 export const LoginPage = () => {
   const { login } = useAuth();
+  useEffect(() => {
+    (async () => {
+      const token = await auth.currentUser?.getIdToken();
+      if (token) {
+        login(token, auth.currentUser?.uid ?? "");
+      }
+    })();
+  }, [login]);
 
   return (
     <div>
